ef29e7e4a9bb9c93636650da3f5f70e4b411b587
maryyj <maria.holster@outlook.com>
2023-11-24 14:33:22 +0100

    add group page

--- ItTakesAVillage/Pages/Group.cshtml.cs
+++ ItTakesAVillage/Pages/Group.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
 
 namespace ItTakesAVillage.Pages
 {
     public class GroupModel : PageModel
     {
+        [BindProperty]
+        public Group? NewGroup { get; set; }
         public void OnGet()
         {
+
         }
     }
 }
