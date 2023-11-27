11154800d89ad5e70d62016b59a048aceda2d845
maryyj <maria.holster@outlook.com>
2023-11-27 10:30:11 +0100

    add functionality to add member to a group

--- ItTakesAVillage/Pages/Group.cshtml.cs
+++ ItTakesAVillage/Pages/Group.cshtml.cs
@@ -1,8 +1,10 @@
 using ItTakesAVillage.Contracts;
+using ItTakesAVillage.Data;
 using ItTakesAVillage.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Text.RegularExpressions;
 
 namespace ItTakesAVillage.Pages
@@ -12,22 +14,29 @@ namespace ItTakesAVillage.Pages
         private readonly IGroupService _groupService;
         private readonly IUserService _userService;
         private readonly UserManager<ItTakesAVillageUser> _userManager;
+        private readonly ItTakesAVillageContext _context;
 
-        [BindProperty]
+        //[BindProperty]
         public ItTakesAVillageUser CurrentUser { get; set; }
         [BindProperty]
         public Models.Group NewGroup { get; set; } = new Models.Group();
+        
+        [BindProperty]
+        public Models.UserGroup NewUserGroup { get; set; } = new Models.UserGroup();
 
-        public GroupModel(IGroupService groupService, IUserService userService, UserManager<ItTakesAVillageUser> userManager)
+        public GroupModel(IGroupService groupService, IUserService userService, UserManager<ItTakesAVillageUser> userManager, ItTakesAVillageContext context)
         {
             _groupService = groupService;
             _userManager = userManager;
             _userService = userService;
+            _context = context;
         }
 
         public async Task<IActionResult> OnGet()
         {
             CurrentUser = await _userManager.GetUserAsync(User);
+            ViewData["UserId"] = new SelectList(_userManager.Users, "Id", "Email");
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Name");
             return Page();
         }
 
@@ -35,21 +44,24 @@ namespace ItTakesAVillage.Pages
         {
             if (ModelState.IsValid)
             {
+                CurrentUser = await _userManager.GetUserAsync(User);
                 var newGroupId = await _groupService.SaveGroup(NewGroup);
-                var user = _userService.GetUserByUserName(User.Identity.Name);
-                if (newGroupId != 0 && user.UserName != null)
+                if (newGroupId != 0 && CurrentUser.Id != null)
                 {
-                    await _groupService.AddUserToGroup(user.Id, newGroupId);
+                    await _groupService.AddUserToGroup(CurrentUser.Id, newGroupId);
                 }
             }
-            return Page();
+            return RedirectToPage("/Group");
         }
 
-        //public async Task<IActionResult> OnPostAddUserToGroupAsync(string userId, int groupId)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //    }
-        //}
+        public async Task<IActionResult> OnPostAddUserToGroupAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                await _groupService.AddUserToGroup(NewUserGroup.UserId, NewUserGroup.GroupId);
+            }
+            return RedirectToPage("/Group");
+        }
+
     }
 }
