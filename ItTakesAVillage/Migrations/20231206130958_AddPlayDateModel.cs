0fde199c0868b7c1da041eddf72eb42f0f65b64c
maryyj <maria.holster@outlook.com>
2023-12-06 14:15:20 +0100

    add playdate model in project and database

---
+++ ItTakesAVillage/Migrations/20231206130958_AddPlayDateModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItTakesAVillage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayDateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChildName",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvitedChildName",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlayDate_Location",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChildName",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "InvitedChildName",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "PlayDate_Location",
+                table: "Events");
+        }
+    }
+}
